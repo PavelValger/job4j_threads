e57da46bcc34bd96b9775e27ac123b74ef2be291
Pavel Valger <pavelwalker@mail.ru>
2023-04-13 21:30:05 +0500

    2. Создание и запуск нити [#1016]

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,17 @@
+package main.java.ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread another = new Thread(
+                    () -> System.out.println(Thread.currentThread().getName())
+            );
+            another.start();
+            Thread second = new Thread(
+                    () -> System.out.println(Thread.currentThread().getName())
+            );
+            second.start();
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
