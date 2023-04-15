df1a08eaaf6a9b1f5e982b3c7b57cd44d823e534
Pavel Valger <pavelwalker@mail.ru>
2023-04-15 15:49:16 +0500

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package main.java.ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 100; i++) {
+                            System.out.printf("\rLoading : %s%%", i);
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
