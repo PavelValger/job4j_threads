52cb050c5d5e27a82b26e9d018e287a374069ce2
Pavel Valger <pavelwalker@mail.ru>
2023-04-18 21:00:37 +0500

    9. ThreadLocal [#504912]

---
+++ src/main/java/ru/job4j/threads/threadlocal/SecondThread.java
@@ -0,0 +1,10 @@
+package main.java.ru.job4j.threads.threadlocal;
+
+public class SecondThread extends Thread {
+
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("Это поток 2.");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+}
