9c005842ee41e64121a2fcd66c3396b543188fd9
Pavel Valger <pavelwalker@mail.ru>
2023-04-21 21:03:39 +0500

    4. Thread без общих ресурсов [#267919] удалил аннотацию @ThreadSafe

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -1,13 +1,10 @@
 package main.java.ru.job4j.ref;
 
-import net.jcip.annotations.ThreadSafe;
-
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-@ThreadSafe
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
