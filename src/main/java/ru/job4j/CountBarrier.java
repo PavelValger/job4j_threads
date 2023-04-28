245aad205521a2ce1c2c72602375a53f0fcd6515
Pavel Valger <pavelwalker@mail.ru>
2023-04-28 21:41:23 +0500

    0. Управление нитью через wait. [#6858] Добавил main и проверил работу класса CountBarrier

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import java.util.function.Consumer;
+
 public class CountBarrier {
     private final int total;
     private int count = 0;
@@ -21,5 +23,35 @@ public class CountBarrier {
                 Thread.currentThread().interrupt();
             }
         }
+    }
+
+    private static void action(Consumer<Integer> consumer) {
+        while (!Thread.currentThread().isInterrupted()) {
+            consumer.accept(0);
+            System.out.printf("%s %s%s", Thread.currentThread().getName(), Thread.currentThread().getState(),
+                    System.lineSeparator());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountBarrier barrier = new CountBarrier(3);
+        Thread master = new Thread(
+                () -> action(integer -> barrier.count()),
+                "Master"
+        );
+        Thread slave = new Thread(
+                () -> action(integer -> barrier.await()),
+                "Slave"
+        );
+        master.start();
+        slave.start();
+        Thread.sleep(10000);
+        master.interrupt();
+        slave.interrupt();
     }
 }
