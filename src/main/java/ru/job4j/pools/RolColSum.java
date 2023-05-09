e5ea2f7f778042df72d0687fb138fd1f0f8ab664
Pavel Valger <pavelwalker@mail.ru>
2023-05-09 22:23:02 +0500

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,114 +1,60 @@
 package ru.job4j.pools;
 
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
-    private static Sums[] sums;
-
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public Sums(int rowSum, int colSum) {
-            this.rowSum = rowSum;
-            this.colSum = colSum;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Sums sums = (Sums) o;
-            return rowSum == sums.rowSum && colSum == sums.colSum;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(rowSum, colSum);
-        }
-
-        @Override
-        public String toString() {
-            return "Sums{rowSum=%d, colSum=%d}".formatted(rowSum, colSum);
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
-        sums = new Sums[matrix.length];
+        Sums[] sums = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             sums[i] = new Sums(0, 0);
+            int rowSum = 0;
+            int colSum = 0;
             for (int j = 0; j < matrix[i].length; j++) {
-                sums[i].rowSum += matrix[i][j];
-                sums[j].colSum += matrix[i][j];
+                rowSum += matrix[i][j];
+                colSum += matrix[j][i];
             }
+            sums[i].setRowSum(rowSum);
+            sums[i].setColSum(colSum);
         }
         return sums;
     }
 
     public static Sums[] asyncSum(int[][] matrix) {
-        thenComposeExample(matrix);
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            count(i, matrix, sums);
+        }
         return sums;
     }
 
-    private static CompletableFuture<Void> createSums(int[][] matrix) {
-        return CompletableFuture.runAsync(
+    private static void count(int i, int[][] matrix, Sums[] sums) {
+        CompletableFuture.runAsync(
                 () -> {
-                    sums = new Sums[matrix.length];
-                    for (int i = 0; i < sums.length; i++) {
-                        sums[i] = new Sums(0, 0);
+                    System.out.println(Thread.currentThread().getName());
+                    sums[i] = new Sums(0, 0);
+                    int rowSum = 0;
+                    int colSum = 0;
+                    for (int j = 0; j < matrix[i].length; j++) {
+                        rowSum += matrix[i][j];
+                        colSum += matrix[j][i];
                     }
-                });
-    }
-
-    private static CompletableFuture<Void> countLines(int[][] matrix) {
-        return CompletableFuture.runAsync(
-                () -> {
-                    for (int i = 0; i < matrix.length; i++) {
-                        for (int j = 0; j < matrix[i].length; j++) {
-                            sums[i].rowSum += matrix[i][j];
-                        }
-                    }
-                });
-    }
-
-    private static CompletableFuture<Void> countColumns(int[][] matrix) {
-        return CompletableFuture.runAsync(
-                () -> {
-                    for (int[] ints : matrix) {
-                        for (int j = 0; j < ints.length; j++) {
-                            sums[j].colSum += ints[j];
-                        }
-                    }
-                });
-    }
-
-    private static CompletableFuture<Void> allOfExample(int[][] matrix) {
-        return CompletableFuture.runAsync(
-                () -> CompletableFuture.allOf(countLines(matrix), countColumns(matrix)));
-    }
-
-    private static void thenComposeExample(int[][] matrix) {
-        CompletableFuture<Void> result = createSums(matrix).thenCompose(a -> allOfExample(matrix));
-        try {
-            result.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+                    sums[i].setRowSum(rowSum);
+                    sums[i].setColSum(colSum);
+                }
+        ).join();
     }
 
     public static void main(String[] args) {
-        int[][] array = {
-                {1},
-                {2, 3},
-                {4, 5, 6}};
-        System.out.println(Arrays.toString(sum(array)));
-        System.out.println(Arrays.toString(asyncSum(array)));
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {4, 5, 6, 5},
+                {7, 8, 9, 5}};
+        int count = 0;
+        while (count != 50) {
+            RolColSum.asyncSum(matrix);
+            count++;
+        }
     }
 }
--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -2,33 +2,35 @@ package ru.job4j.pools;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RolColSumTest {
 
     @Test
     void thenSum() {
         int[][] matrix = {
-                {1},
-                {2, 3},
-                {4, 5, 6}};
-        RolColSum.Sums exp1 = new RolColSum.Sums(1, 7);
-        RolColSum.Sums exp2 = new RolColSum.Sums(5, 8);
-        RolColSum.Sums exp3 = new RolColSum.Sums(15, 6);
-        RolColSum.Sums[] expected = {exp1, exp2, exp3};
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        Sums exp1 = new Sums(6, 12);
+        Sums exp2 = new Sums(15, 15);
+        Sums exp3 = new Sums(24, 18);
+        Sums[] expected = {exp1, exp2, exp3};
         assertThat(RolColSum.sum(matrix)).containsExactly(expected);
     }
 
     @Test
     void thenAsyncSum() {
         int[][] matrix = {
-                {1},
-                {2, 3},
-                {4, 5, 6}};
-        RolColSum.Sums exp1 = new RolColSum.Sums(1, 7);
-        RolColSum.Sums exp2 = new RolColSum.Sums(5, 8);
-        RolColSum.Sums exp3 = new RolColSum.Sums(15, 6);
-        RolColSum.Sums[] expected = {exp1, exp2, exp3};
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {4, 5, 6, 5},
+                {7, 8, 9, 5}};
+        Sums exp1 = new Sums(10, 13);
+        Sums exp2 = new Sums(10, 17);
+        Sums exp3 = new Sums(20, 21);
+        Sums exp4 = new Sums(29, 18);
+        Sums[] expected = {exp1, exp2, exp3, exp4};
         assertThat(RolColSum.asyncSum(matrix)).containsExactly(expected);
     }
 }
