d52bff9a4381584227aba50fa66d791aa1956634
Pavel Valger <pavelwalker@mail.ru>
2023-05-10 19:00:29 +0500

    4. CompletableFuture [#361626] рефакторинг

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -4,23 +4,19 @@ import java.util.concurrent.CompletableFuture;
 
 public class RolColSum {
 
-    public static Sums[] sum(int[][] matrix) {
-        Sums[] sums = new Sums[matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            sums[i] = new Sums(0, 0);
-            int rowSum = 0;
-            int colSum = 0;
-            for (int j = 0; j < matrix[i].length; j++) {
-                rowSum += matrix[i][j];
-                colSum += matrix[j][i];
-            }
-            sums[i].setRowSum(rowSum);
-            sums[i].setColSum(colSum);
+    private static void count(int i, int[][] matrix, Sums[] sums) {
+        sums[i] = new Sums(0, 0);
+        int rowSum = 0;
+        int colSum = 0;
+        for (int j = 0; j < matrix[i].length; j++) {
+            rowSum += matrix[i][j];
+            colSum += matrix[j][i];
         }
-        return sums;
+        sums[i].setRowSum(rowSum);
+        sums[i].setColSum(colSum);
     }
 
-    public static Sums[] asyncSum(int[][] matrix) {
+    public static Sums[] sum(int[][] matrix) {
         Sums[] sums = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             count(i, matrix, sums);
@@ -28,33 +24,13 @@ public class RolColSum {
         return sums;
     }
 
-    private static void count(int i, int[][] matrix, Sums[] sums) {
-        CompletableFuture.runAsync(
-                () -> {
-                    System.out.println(Thread.currentThread().getName());
-                    sums[i] = new Sums(0, 0);
-                    int rowSum = 0;
-                    int colSum = 0;
-                    for (int j = 0; j < matrix[i].length; j++) {
-                        rowSum += matrix[i][j];
-                        colSum += matrix[j][i];
-                    }
-                    sums[i].setRowSum(rowSum);
-                    sums[i].setColSum(colSum);
-                }
-        ).join();
-    }
-
-    public static void main(String[] args) {
-        int[][] matrix = {
-                {1, 2, 3, 4},
-                {1, 2, 3, 4},
-                {4, 5, 6, 5},
-                {7, 8, 9, 5}};
-        int count = 0;
-        while (count != 50) {
-            RolColSum.asyncSum(matrix);
-            count++;
+    public static Sums[] asyncSum(int[][] matrix) {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            int finalIndex = i;
+            CompletableFuture.runAsync(
+                    () -> count(finalIndex, matrix, sums)).join();
         }
+        return sums;
     }
 }
